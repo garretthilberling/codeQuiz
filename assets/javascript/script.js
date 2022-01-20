df28040646468c1580b1f2b5f14cf64a4615fecb
garretthilberling <ghilberling@knights.ucf.edu>
2022-01-20 12:44:12 -0500

    I went ahead and added the timer, having it count down from 120, and set the function that displays incorrect to also deduct 10 points from the timer.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,6 +10,8 @@ var questionSection = document.querySelector("#insert-question");
 
 var answerSection = document.querySelector("#answer");
 
+var timerSection = document.querySelector("#timer");
+
 var nameWordArray = [
   "leaf",
   "skeleton",
@@ -27,6 +29,7 @@ var nameWordArray = [
   "echo",
   "favourite",
   "big-brain",
+  "boywonder",
 ];
 var nameColorArray = [
   "red",
@@ -185,7 +188,7 @@ var goodLuckMessage = function () {
   answerSection.appendChild(goodLuckText);
   var fade = setInterval(function () {
     if (!goodLuckText.style.opacity) {
-      $("#good-luck").fadeOut(8000, function () {
+      $("#good-luck").fadeOut(4000, function () {
         $(this).remove();
       });
     } else {
@@ -217,9 +220,69 @@ var submitName = function (e) {
   };
 };
 
+var startTimer = function () {
+  var timeText = document.querySelector("#timer");
+  timeText.innerHTML = "120";
+  var countdown = setInterval(displayTimer, 1000, timeText);
+
+  function displayTimer(element) {
+    var t = element.innerHTML;
+    if (t > 0) {
+      element.innerHTML = parseInt(t) - 1;
+    } 
+    // else if (incorrectAnswer === true) {
+    //   element.innerHTML = parseInt(t) - 10;
+    // } 
+    else {
+      element.innerHTML = 0;
+    }
+    window.t = t;
+    window.element = element;
+  }
+};
+
+var correctAnswer = function () {
+  const correctText = document.createElement("div");
+  correctText.className = "answer-parent result";
+  // correctText.firstElementChild
+  correctText.innerHTML = "<p>Correct!</p>";
+  correctText.setAttribute("id", "correct");
+  answerSection.appendChild(correctText);
+
+  var fade = setInterval(function () {
+    if (!correctText.style.opacity) {
+      $("#correct").fadeOut(2000, function () {
+        $(this).remove();
+      });
+    } else {
+      clearInterval(fade);
+    }
+  });
+};
+
+var incorrectAnswer = function () {
+  const incorrectText = document.createElement("div");
+  incorrectText.className = "answer-parent result";
+  // incorrectText.firstElementChild
+  incorrectText.innerHTML = '<p class="incorrect">Incorrect</p>';
+  incorrectText.setAttribute("id", "incorrect");
+  answerSection.appendChild(incorrectText);
+  var fade = setInterval(function () {
+    if (!incorrectText.style.opacity) {
+      $("#incorrect").fadeOut(2000, function () {
+        $(this).remove();
+      });
+    } else {
+      clearInterval(fade);
+    }
+  });
+  element.innerHTML = parseInt(t) - 10;
+};
+
 var startQuiz = function (e) {
-  if (generateName === false || submitName === false) {
+  if (userName === "") {
     window.alert("You need to provide a valid name to proceed!");
+    return false;
   }
   var firstPage = document.getElementById("question");
   firstPage.remove();
@@ -234,8 +297,129 @@ var startQuiz = function (e) {
   questionOne.setAttribute("id", "question");
   questionOne.innerHTML = "<p>In CSS, what does '!important' do?</p>";
   questionSection.appendChild(questionOne);
-  // var questionOne = document.createElement("span");
-  // questionOne.innerHTML = "<p>Good luck, " + userName + "!";
+
+  const questionOneAnswers = document.createElement("span");
+  questionOneAnswers.setAttribute("id", "answer");
+  questionOneAnswers.innerHTML =
+    '<div id="answer-parent" class="answer-parent"><button id="answer-1" class="answer-container">A</button></div> <div id="answer-parent" class="answer-parent"><button id="answer-2" class="answer-container">B</button></div> <div id="answer-parent" class="answer-parent"><button id="answer-3" class="answer-container">C</button></div> <div id="answer-parent" class="answer-parent"><button id="answer-4" class="answer-container">D</button></div>';
+  answerSection.appendChild(questionOneAnswers);
+
+  var questionOneCorrect = document.querySelector("#answer-3");
+  var questionOneIncorrect = document.querySelector(
+    "#answer-1, #answer-2, #answer-4"
+  );
+  var proceed = document.querySelector(
+    "#answer-1, #answer-2, #answer-3, #answer-4"
+  );
+
+  questionOneCorrect.addEventListener("click", correctAnswer);
+  questionOneIncorrect.addEventListener("click", incorrectAnswer);
+
+  var questionTwo = function () {
+    questionOne.remove();
+    questionOneAnswers.remove();
+    const questionTwo = document.createElement("span");
+
+    questionTwo.setAttribute("id", "question");
+    questionTwo.innerHTML = "<p>Question 2</p>";
+    questionSection.appendChild(questionTwo);
+
+    const questionTwoAnswers = document.createElement("span");
+    questionTwoAnswers.setAttribute("id", "answer");
+    questionTwoAnswers.innerHTML =
+      '<div id="answer-parent" class="answer-parent"><button id="answer-1" class="answer-container">A</button></div> <div id="answer-parent" class="answer-parent"><button id="answer-2" class="answer-container">B</button></div> <div id="answer-parent" class="answer-parent"><button id="answer-3" class="answer-container">C</button></div> <div id="answer-parent" class="answer-parent"><button id="answer-4" class="answer-container">D</button></div>';
+    answerSection.appendChild(questionTwoAnswers);
+
+    var questionTwoCorrect = document.querySelector("#answer-1");
+    var questionTwoIncorrect = document.querySelector(
+      "#answer-2, #answer-3, #answer-4"
+    );
+    var proceed2 = document.querySelector(
+      "#answer-1, #answer-2, #answer-3, #answer-4"
+    );
+    // globalThis.proceed2
+
+    questionTwoCorrect.addEventListener("click", correctAnswer);
+    questionTwoIncorrect.addEventListener("click", incorrectAnswer);
+  };
+  var questionThree = function () {
+    document.querySelector("#question").remove();
+    document.querySelector("#answer").remove();
+    const questionThree = document.createElement("span");
+
+    questionThree.setAttribute("id", "question");
+    questionThree.innerHTML = "<p>Question 2</p>";
+    questionSection.appendChild(questionThree);
+
+    const questionThreeAnswers = document.createElement("span");
+    questionThreeAnswers.setAttribute("id", "answer");
+    questionThreeAnswers.innerHTML =
+      '<div id="answer-parent" class="answer-parent"><button id="answer-1" class="answer-container">A</button></div> <div id="answer-parent" class="answer-parent"><button id="answer-2" class="answer-container">B</button></div> <div id="answer-parent" class="answer-parent"><button id="answer-3" class="answer-container">C</button></div> <div id="answer-parent" class="answer-parent"><button id="answer-4" class="answer-container">D</button></div>';
+    answerSection.appendChild(questionThreeAnswers);
+
+    var questionThreeCorrect = document.querySelector("#answer-4");
+    var questionThreeIncorrect = document.querySelector(
+      "#answer-1, #answer-2, #answer-3"
+    );
+
+    questionThreeCorrect.addEventListener("click", correctAnswer);
+    questionThreeIncorrect.addEventListener("click", incorrectAnswer);
+  };
+  var questionFour = function () {
+    document.querySelector("#question").remove();
+    document.querySelector("#answer").remove();
+    const questionFour = document.createElement("span");
+
+    questionFour.setAttribute("id", "question");
+    questionFour.innerHTML = "<p>Question 2</p>";
+    questionSection.appendChild(questionFour);
+
+    const questionFourAnswers = document.createElement("span");
+    questionFourAnswers.setAttribute("id", "answer");
+    questionFourAnswers.innerHTML =
+      '<div id="answer-parent" class="answer-parent"><button id="answer-1" class="answer-container">A</button></div> <div id="answer-parent" class="answer-parent"><button id="answer-2" class="answer-container">B</button></div> <div id="answer-parent" class="answer-parent"><button id="answer-3" class="answer-container">C</button></div> <div id="answer-parent" class="answer-parent"><button id="answer-4" class="answer-container">D</button></div>';
+    answerSection.appendChild(questionFourAnswers);
+
+    var questionFourCorrect = document.querySelector("#answer-2");
+    var questionFourIncorrect = document.querySelector(
+      "#answer-1, #answer-3, #answer-4"
+    );
+
+    questionFourCorrect.addEventListener("click", correctAnswer);
+    questionFourIncorrect.addEventListener("click", incorrectAnswer);
+  };
+  // proceed.addEventListener("click", questionTwo);
+  // proceed2.addEventListener("click", questionThree);
+  // proceed.addEventListener("click", questionFour);
+  // proceed4.addEventListener("click", questionFive);
+  // proceed5.addEventListener("click", questionSix);
+  // proceed6.addEventListener("click", questionSeven);
+  // proceed7.addEventListener("click", questionEight);
+  // proceed8.addEventListener("click", questionNine);
+  // proceed9.addEventListener("click", questionTen);
+
+  // if (answerOneBtn) {
+  //   answerOneBtn.addEventListener("click", modifyText, true);
+  // }
+  // if (answerTwoBtn) {
+  //   answerTwoBtn.addEventListener("click", modifyText, true);
+  // }
+  // if (answerThreeBtn) {
+  //   answerThreeBtn.addEventListener("click", modifyText, true);
+  // }
+  // if (answerFourBtn) {
+  //   answerOneBtn.addEventListener("click", modifyText, true);
+  // }
+
+  // if (answerThreeBtn === true) {
+  //   correctAnswer();
+  // } else if (
+  //   answerOneBtn === true ||
+  //   answerTwoBtn === true ||
+  //   answerFourBtn === true
+  // ) {
+  //   incorrectAnswer();
+  // }
 };
 
 generateBtn.addEventListener("click", generateName);
@@ -243,3 +427,5 @@ generateBtn.addEventListener("click", generateName);
 submitBtn.addEventListener("click", submitName);
 
 startQuizBtn.addEventListener("click", startQuiz);
+
+startQuizBtn.addEventListener("click", startTimer);
